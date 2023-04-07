6cc04d3ed188f32cf151e539ffcb5b59147f1456
Aspheria <lorran.parrilla@gmail.com>
2023-04-07 19:04:15 -0300

    feat: add new types

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Type } from '../_model/type';
+import { Pokemon } from '../_model/pokemon';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-list.component.sass']
 })
 export class PokemonListComponent {
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: "Bulba",
+      types: [
+        Type.Grass,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      number: 2,
+      name: "Ivysauro",
+      types: [
+        Type.Grass,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+      number: 3,
+      name: "Venosauro",
+      types: [
+        Type.Grass,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
+      number: 3,
+      name: "Charmander",
+      types: [
+        Type.Fire,
+        Type.Poison,
+      ],
+    },
+  ];
 
 }
